049587dd529e782732bde23fb613a4aac10df315
John Finley <johnfinleybremerton@gmail.com>
2022-08-01 13:13:49 -0600

    .

---
+++ src/Favorites.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Favorites({ favorites }) {
+  const favoritesCards = favorites.map((favorite) => (
+    <span key={favorite.id}>
+  
+      <button
+        style={{
+          height: "12vh",
+          width: "80%",
+          fontSize: "1em",
+          marginTop: "5px",
+        }}
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        {favorite.city}
+      </button>
+    </span>
+  ));
+
+  return (
+    <div id="favorites">
+      
+      <div id="favoritesList">{favoritesCards}</div>
+    </div>
+  );
+}
+
+export default Favorites;
---
+++ src/Filters.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Filters() {
+  return (
+    <div id="filters">
+      <form>
+        <div className="relative">
+          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-black dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search Mockups, Logos..."
+          />
+        </div>
+      </form>
+      <select
+        id="countries"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option>Sort by Name</option>
+        <option value="US">A - Z</option>
+        <option value="CA">Z - A</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filters;
