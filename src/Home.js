f5af290d9707105e7649534f3e20fbe3e3fab450
John Finley <johnfinleybremerton@gmail.com>
2022-08-03 12:08:53 -0600

    Loads current weather data for first city in favorites

--- src/Home.js
+++ src/Home.js
@@ -5,42 +5,43 @@ import Content from "./Content";
 
 function Home() {
   const [favorites, setFavorites] = useState([]);
-  const [currentCity, setCurrentCity] = useState("")
-  const [search, setSearch] = useState("")
-  const [sort, setSort] = useState()
+  const [currentCity, setCurrentCity] = useState("");
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
     if (localStorage.getItem("cities") === null) {
       fetch("http://localhost:3000/cities")
         .then((res) => res.json())
         .then((data) => {
           setFavorites(data);
-          setCurrentCity(data[0].city)
+          setCurrentCity(data[0].city);
           localStorage.setItem("cities", JSON.stringify(data));
         });
     } else {
-      setFavorites(JSON.parse(localStorage.getItem("cities")))
+      setFavorites(JSON.parse(localStorage.getItem("cities")));
       setCurrentCity(JSON.parse(localStorage.getItem("cities"))[0].city);
-
     }
   }, []);
 
   function handleSearch(e) {
-    setSearch(e)
+    setSearch(e);
   }
 
-  const displayedFavorite = favorites.filter((favorite) => favorite.city.toLowerCase().includes(search.toLowerCase()))
-
-  function handleSort(e) {
-
-  }
+  const displayedFavorite = favorites.filter((favorite) =>
+    favorite.city.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div id="home">
       <div id="favoritesContainer">
         <Filters handleSearch={handleSearch} />
-        <Favorites favorites={displayedFavorite} />
+        <Favorites
+          favorites={displayedFavorite}
+        />
       </div>
-      <div id="contentContainer"><Content /></div>
+      <div id="contentContainer">
+        <Content currentCity={currentCity} />
+      </div>
     </div>
   );
 }
