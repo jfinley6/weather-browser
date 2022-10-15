9474f7bb1687f5416deb1deb3c320d2125231159
johnfinley <johnfinleybremerton@gmail.com>
2022-10-15 13:49:21 -0600

    state now displayed

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Favorites({ favorites, setCurrentCity }) {
-
-  function handleFavorite(value){
-setCurrentCity(value)
+function Favorites({ favorites, setCurrentCity, setCurrentState }) {
+  function handleFavorite(value) {
+    setCurrentCity(value.city);
+    setCurrentState(value.state)
   }
   const favoritesCards = favorites.map((favorite) => (
     <span key={favorite.city}>
-  
-      <button onClick={() => handleFavorite(favorite.city)}
+      <button
+        onClick={() => handleFavorite(favorite)}
         style={{
           height: "12vh",
           width: "80%",
@@ -18,14 +18,13 @@ setCurrentCity(value)
         id="favoriteCard"
         className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
       >
-        {favorite.city}
+        {favorite.city}, {favorite.state}
       </button>
     </span>
   ));
 
   return (
     <div id="favorites">
-      
       <div id="favoritesList">{favoritesCards}</div>
     </div>
   );
