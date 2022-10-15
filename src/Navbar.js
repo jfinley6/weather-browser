ffde94b0a0f9a9568159901eaa4bfb25cf0d992a
johnfinley <johnfinleybremerton@gmail.com>
2022-10-15 15:46:22 -0600

    logo now redirects to the main page

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const history = useHistory()
   return (
     <nav className="navbar">
-      <h1>React Weather App</h1>
+      <h1 role="button" onClick={() => history.push("/")}>React Weather App</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
